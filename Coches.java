817f2907d952396cd544e3c22f3dcf707bbb872c
feermorales <feernando_morales@outlook.es>
2020-03-27 20:39:01 +0000

    Está es la aportación de Feer

--- Coches.java
+++ Coches.java
@@ -20,6 +20,11 @@ public static void Audi(){
         System.out.println("Cuatro caballos de fuerza");
         System.out.println("Costo :$2,800,200");  
  }
+public static void BMW(){ 
+        System.out.println("Automovil BMW");
+        System.out.println("Seis caballos de fuerza");
+        System.out.println("Costo :$1,560,899");  
+ }
 
 
 public static void main(String args[]){
@@ -27,7 +32,7 @@ public static void main(String args[]){
 	System.out.println("1.- Mercedez");
 	System.out.println("2.-Ferrari");
 	System.out.println("3.- Audi");
-
+System.out.println("4.- BMW");
 
 x = entrada.nextInt();
 
@@ -36,6 +41,7 @@ switch(x){
 	case 1: Mercedez(); break;
 	case 2: Ferrari(); break;
 	case 3: Audi(); break;
+case 4: BMW(); break;
 
 default: System.out.println("Numero erroneo"); break;
 
